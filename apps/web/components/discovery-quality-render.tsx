bd27b4ecd47cc783609b8b43aa17aa46ad2ad8d6
sohaib <sohaib.benamor@gmail.com>
2026-04-16 16:31:50 +0100

    feat: harden finance discovery quality

---
+++ apps/web/components/discovery-quality-render.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import type { MissionDetailView } from "@pocket-cto/domain";
+import { DiscoveryAnswerCard } from "./discovery-answer-card";
+import { MissionCard } from "./mission-card";
+
+type MissionCardRenderInput = Pick<
+  MissionDetailView,
+  | "approvalCards"
+  | "artifacts"
+  | "discoveryAnswer"
+  | "liveControl"
+  | "mission"
+  | "proofBundle"
+  | "tasks"
+>;
+
+export function renderDiscoveryAnswerCardMarkup(input: {
+  answer: MissionDetailView["discoveryAnswer"];
+  mission: MissionDetailView["mission"];
+}) {
+  return renderToStaticMarkup(
+    <DiscoveryAnswerCard answer={input.answer} mission={input.mission} />,
+  );
+}
+
+export function renderMissionCardMarkup(input: MissionCardRenderInput) {
+  return renderToStaticMarkup(<MissionCard {...input} />);
+}
